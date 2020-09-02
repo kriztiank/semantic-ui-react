5bec737bae16ac281fe1fc2bff05f022072f7b08
kriztiank <kriztiank@gmail.com>
2020-09-02 18:06:19 +0000

    display state in the header

--- src/App.js
+++ src/App.js
@@ -1,61 +1,41 @@
 import React from "react";
 import HomeHeader from "./components/HomeHeader";
-// import { Input, Segment } from "semantic-ui-react";
+import { Input, Segment } from "semantic-ui-react";
 import "./index.css";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            author: 'kriztiank',
-            name: "Home"
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      author: "kriztiank",
+      name: "Home",
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event, data) {
+    // console.log(event);
+    // console.log(data);
+    console.log(data.value);
+    this.setState({
+      name: data.value,
+    });
+  }
 
   render() {
     return (
       <div>
-        <HomeHeader name={this.state.name} author={this.state.author} />
+        <HomeHeader author={this.state.author} name={this.state.name} />
+        <Segment basic>
+          <Input
+            label={"Input Form"}
+            onChange={this.onChange}
+            value={this.state.name}
+          />
+        </Segment>
       </div>
     );
   }
 }
 
 export default App;
-
-// class App extends React.Component {
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             author: '@atypical.engineer',
-//             name: "Greg"
-//         }
-//         this.onChange = this.onChange.bind(this)
-//     }
-
-//     onChange(event, data) {
-//         this.setState({
-//             name: data.value
-//         })
-//     }
-
-//     render() {
-//         return (
-//             <div className="App">
-//                 <PokedexHeader
-//                     author={this.state.author}
-//                     name={this.state.name}
-//                 />
-//                 <Segment basic>
-//                     <Input
-//                         label={"Input Form"}
-//                         onChange={this.onChange}
-//                         value={this.state.name}
-//                     />
-//                 </Segment>
-//             </div>
-//         );
-//     }
-// }
-
-// export default App;
