833247b860b7eb13f5dfc7ffc84b0c252a0eeb51
kriztiank <kriztiank@gmail.com>
2020-09-02 17:01:54 +0000

    adding state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,61 @@
 import React from "react";
 import HomeHeader from "./components/HomeHeader";
+// import { Input, Segment } from "semantic-ui-react";
 import "./index.css";
 
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            author: 'kriztiank',
+            name: "Home"
+        }
+    }
 
-export default function App() {
-    return <HomeHeader name={'Home'} author={'kriztiank'}/>
+  render() {
+    return (
+      <div>
+        <HomeHeader name={this.state.name} author={this.state.author} />
+      </div>
+    );
+  }
 }
+
+export default App;
+
+// class App extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             author: '@atypical.engineer',
+//             name: "Greg"
+//         }
+//         this.onChange = this.onChange.bind(this)
+//     }
+
+//     onChange(event, data) {
+//         this.setState({
+//             name: data.value
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div className="App">
+//                 <PokedexHeader
+//                     author={this.state.author}
+//                     name={this.state.name}
+//                 />
+//                 <Segment basic>
+//                     <Input
+//                         label={"Input Form"}
+//                         onChange={this.onChange}
+//                         value={this.state.name}
+//                     />
+//                 </Segment>
+//             </div>
+//         );
+//     }
+// }
+
+// export default App;
